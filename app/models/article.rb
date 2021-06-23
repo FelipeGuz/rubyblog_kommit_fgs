7e720e58dfb9b6677965bd93d629778c7949400e
Felipe <felipe.guzman.kommit@gmail.com>
2021-06-23 12:20:09 -0500

    Adding concerns to the articles and comments status

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,10 @@
 class Article < ApplicationRecord
+    include Visible
+    
+    # Relations
     has_many :comments
 
+    # Condition verification
     validates :title, presence: true
     validates :body, presence: true, length: {minimum: 10}
 end
