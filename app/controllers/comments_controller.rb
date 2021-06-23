74c033291d8daa91d8cd4cc4598bec19297476ff
Felipe <felipe.guzman.kommit@gmail.com>
2021-06-23 14:15:33 -0500

    Added a security layer to add or delete new articles and delete comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,8 @@
 class CommentsController < ApplicationController
+
+    # security authentication
+    http_basic_authenticate_with name: 'dhh', password: 'secret', only: :destroy
+
     def create
         @article = Article.find(params[:article_id])
         @comments = @article.comments.create(comment_params)
