137629646622d08ac45a5d817ec0b9f7c57758f8
Felipe <felipe.guzman.kommit@gmail.com>
2021-06-30 19:54:21 -0500

    Solving dates error
    Solving design errors
    
    Chenge color of the modal/alert of _modal.html.erb file
    
    Solving another error

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,10 @@
 # Load the Rails application.
-require_relative "application"
+require_relative 'application'
+
+# Config of errors in rails
+ActionView::Base.field_error_proc = proc do |html_tag, _|
+  html_tag.html_safe
+end
 
 # Initialize the Rails application.
 Rails.application.initialize!
