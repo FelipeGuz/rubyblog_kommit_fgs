bbc076c20e5d429513abba984f2c88f7f7b6e6b9
Felipe <felipe.guzman.kommit@gmail.com>
2021-07-02 12:43:12 -0500

    Solved NaN score error
    Solved new/edir article view title linked to root

--- app/models/article.rb
+++ app/models/article.rb
@@ -12,4 +12,12 @@ class Article < ApplicationRecord
   validates :user_id, presence: true
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
+
+  # Define comments score method
+  def score
+    l = comments.length.to_f
+    return 0 if l.zero?
+
+    (comments.reduce(0) { |p, a| p + a.score } / l).round(1)
+  end
 end
